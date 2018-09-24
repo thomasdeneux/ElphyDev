fe754d4a3483c86043ed51d6e3eb5ebbc733c04d
thomas bureau <thomas.deneux@unic.cnrs-gif.fr>
2018-09-24 14:04:12 +0200

    update scripts to create the zip file updates: put the accurate needed cuda dlls

--- Scripts Windows/ZipElphy2.bat
+++ Scripts Windows/ZipElphy2.bat
@@ -8,8 +8,6 @@ copy Elphy2_DX9.exe Elphy2.exe
 
 "C:\Program Files\7-Zip\7z" a ..\Setup\Elphy2.zip cuda\cuda65.dll
 "C:\Program Files\7-Zip\7z" a ..\Setup\Elphy2.zip cuda\cudaMC65.dll
-"C:\Program Files\7-Zip\7z" a ..\Setup\Elphy2.zip cuda\cuda80.dll
-"C:\Program Files\7-Zip\7z" a ..\Setup\Elphy2.zip cuda\cudaMC80.dll
 
 "C:\Program Files\7-Zip\7z" a ..\Setup\Elphy2.zip Elphy.txt
 
--- Scripts Windows/ZipElphy64.bat
+++ Scripts Windows/ZipElphy64.bat
@@ -4,8 +4,8 @@ cd ..\DelpheXE\win64\bin
 "C:\Program Files\7-Zip\7z" a ..\Setup\Elphy64.zip Elphy2.prc
 "C:\Program Files\7-Zip\7z" a ..\Setup\Elphy64.zip elphy.chm
 
-"C:\Program Files\7-Zip\7z" a ..\Setup\Elphy64.zip cuda\cuda65.dll
-"C:\Program Files\7-Zip\7z" a ..\Setup\Elphy64.zip cuda\cudaMC65.dll
+"C:\Program Files\7-Zip\7z" a ..\Setup\Elphy64.zip cuda\cuda80.dll
+"C:\Program Files\7-Zip\7z" a ..\Setup\Elphy64.zip cuda\cudaMC80.dll
 
 
 pause
